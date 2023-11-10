1e3f01669196d8e08428f2eb15d5d662d5c1b4b9
aswin-codes <codicon.co@gmail.com>
2023-11-10 18:59:44 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const http = require('http');
+const WebSocket = require('ws');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
+    console.log(message)
+    // Broadcast received message to all clients
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+});
+
+server.listen(8080, () => {
+  console.log('WebSocket server is running on port 8080');
+});
